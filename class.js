cae30e3a0b86abb38cd38938e85d04b89c58801b
chemazu <chukwuemekachemazu@gmail.com>
2022-01-01 17:56:00 +0100

    JS Classes

---
+++ class.js
@@ -0,0 +1,28 @@
+// classes are used to create objecs
+class Rectangle{
+    //setup
+    constructor(width,height,color){
+
+        console.log("create my rectangle ")
+        this.width=width
+        this.length=height
+        this.color=color
+        //the this keyword refers to the object created
+
+
+
+    }
+    getArea(){
+        return this.width * this.length
+    }
+    printDescription(){
+        console.log (`i am a  ${this.color} rectangle of width ${this.width} and length of  ${this.length}`)
+    }
+//constructors are ran when the object is being created
+
+}
+let myRectangle =new Rectangle(5,3,"yellow")
+let myRectangle2 =new Rectangle(2,4,"purple")
+
+console.log(myRectangle2.getArea())
+myRectangle2.printDescription()
