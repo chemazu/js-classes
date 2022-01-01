7c686f5e4f2dfea16758c07348c1c26b20cc6e2e
chemazu <chukwuemekachemazu@gmail.com>
2022-01-01 23:54:47 +0100

    polymorphism

--- class.js
+++ class.js
@@ -1,40 +1,30 @@
-//Inheritance and Extends
+//polymorphism: this is when an instance overides a method in the parent class
 
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+class Animal {
+    constructor(name, age) {
+      this.name = name;
+    }
+    makeSound() {
+      console.log(`Generic animal sounds`);
+    }
+    runAround() {
+        console.log("run around");
+      }
+    //   this will be overwritten
+  
   }
-  describe() {
-    console.log(`i am ${this.name} and i am ${this.age} years old`);
-  }
-}
-
-// Programmer extends Person it inherits all the feature of Person
-class Programmer extends Person {
-  constructor(name, age, experience) {
-    super(name, age)
-
-    this.name = name;
-    this.age = age;
-    this.experience = experience;
-    //custom cbehavior
-    
-
-  }
-  describeProgrammer(){
-    console.log(`i am ${this.name} and i am ${this.age} years old with ${this.experience} years of programming`);
-  }
-}
-
-const array1 = ['a', 'b', 'c'];
-
-
-
-
-
-let person1 = new Person("Mazu", 24)
-person1.describe()
- let programmer1 = new Programmer("Mazu", 24,40)
-programmer1.describe()
-programmer1.describeProgrammer()
+  
+  // Dog extends Animal
+  class Dog extends Animal {
+    constructor(name) {
+      super(name)
+  
+    }
+    runAround(){
+        super.runAround()
+        console.log("doesnt run around")
+    }
+   }
+   let bigDog = new Dog("paul")
+  bigDog.makeSound()
+  bigDog.runAround()
